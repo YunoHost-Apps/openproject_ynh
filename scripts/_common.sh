112eae532480dfe35f8bcf6732be74db50652941
Didier Chavaroche <didier@chavaroche-si.fr>
2025-10-27 20:57:22 +0100

    essai de la version 16

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,9 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+
+manual_install_psql16() {
+    deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+    apt update
+}
